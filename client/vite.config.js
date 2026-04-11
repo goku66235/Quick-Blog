38cf745164863092bc1c46916750113aac04a214
Arbind Kumar Roy <your_github_email@example.com>
2026-04-11 23:29:58 +0530

    🔥 production build ready

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,25 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
   build: {
-    outDir: '../server/public', // ✅ output to backend
+    outDir: '../server/public',
     emptyOutDir: true,
-
-    chunkSizeWarningLimit: 1000, // ✅ remove warning
 
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'] // ✅ better performance
-        }
+        manualChunks: undefined // ✅ FIX
       }
-    }
-  },
-
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000" // ✅ dev API fix
     }
   }
 })
